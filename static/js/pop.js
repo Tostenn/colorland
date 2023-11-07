1563b716c6172d0ab17ad726dc85a6b149f68302
tosten <kouyatosten@gmail.com>
2023-11-07 14:47:44 +0100

    nouvelle class Popup pour gerer les pop

--- static/js/pop.js
+++ static/js/pop.js
@@ -1,3 +1,18 @@
 import {  } from "./fonction.js";
 
 
+export class Popup{
+    /**
+     * ggestion des popup
+     * @param {HTMLElement} parent 
+     * @param {HTMLElement} copyButton 
+     */
+    constructor(parent,copyButton){
+        this.parent = parent
+        this.copyButton = copyButton
+
+        // on récupére le templates
+        this.template = this.parent.querySelector('template')
+        console.log(this.template);
+    }
+}
